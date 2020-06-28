0693fbe5e9b00f135794cb1f2cf659849683666b
ivnard <ivan.nardini@sas.com>
2020-06-28 19:56:22 +0200

    proprocessing script to test

--- src/procedural_ml_pipe/preprocess.py
+++ src/procedural_ml_pipe/preprocess.py
@@ -12,6 +12,9 @@ from sklearn.ensemble import RandomForestClassifier
 from skl2onnx import convert_sklearn
 from skl2onnx.common.data_types import FloatTensorType
 
+#Utils
+import joblib
+
 # Preprocessing functions
 
 def data_loader(datapath):
@@ -41,61 +44,50 @@ def missing_imputer(data, var, replace='missing'):
     '''
     return data[var].replace('?', replace)
 
-def target_encoder(data, target):
+def encoder(data, var, mapping):
     '''
-    Encodes target variable
-    :params data, target
-    :return: Series
+    Encode all variables for training
+    :params: data, var, mapping
+    :return: DataFrame
     '''
-    target_labels = set(data[target])
-    target_labels_dic = {label: index for index, label in enumerate(target_labels, 0)}
-    return data[target].map(target_labels_dic).astype('category')
+    return data[var].map(mapping)
 
-def umbrella_encoder(data, umbrella):
+def dumminizer(data, columns_to_dummies):
     '''
-    Filter for data with positive umbrella limit
-    :params data, umbrella
-    :return DataFrame
+    Generate dummies for nominal variables
+    :params: data, columns_to_dummies
+    :return: DataFrame
     '''
-    flt = data[umbrella]>=0
-    return data[flt]
+    data = pd.get_dummies(data, columns=columns_to_dummies)
+    return data
 
-def numerical_encoder(data, var, ):
-    pass
-    # #discrete variables
-    # if var == 'policy_deductable':
-    #     bins = list(np.linspace(0,2000, 5, dtype = int))
-    #     bin_labels = ['0-500', '501-1000', '1001-1500', '1501-2000']
-    #     new_variable_name = "_".join(['policy_deductable', 'groups'])
-    #     data[new_variable_name] = pd.cut(data['policy_deductable'], bins = bins, labels = bin_labels)
-    #     data.drop('policy_deductable', axis=1, inplace=True)
-    
-    # bin_labels = ['15-20', '21-25', '26-30', '31-35', '36-40', '41-45', '46-50', '51-55', '56-60', '61-65']
-    # bins = [15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65]
-    # new_variable_name = "_".join(['age', 'groups'])
-    # data[new_variable_name] = pd.cut(data['age'], bins = bins, labels = bin_labels, include_lowest = True)
-    # data.drop('age', axis=1, inplace=True)
-    
-    # bins = list(np.linspace(0,2500, 6, dtype = int))
-    # bin_labels = ['very low', 'low', 'medium', 'high', 'very high']
-    # new_variable_name = "_".join(['policy_annual_premium', 'groups'])
-    # data[new_variable_name] = pd.cut(data['policy_annual_premium'], bins = bins, labels=bin_labels)
-    # data.drop('policy_annual_premium', axis=1, inplace=True)
-    
-    # bins = [0, 50, 100, 150, 200, 250, 300, 350, 400, 450, 500]
-    # bin_labels = ['0-50','51-100','101-150','151-200','201-250','251-300','301-350','351-400','401-450','451-500']
-    # new_variable_name = "_".join(['months_as_customer', 'groups'])
-    # data[new_variable_name] = pd.cut(data['months_as_customer'], bins = 10, labels = bin_labels, include_lowest= True)
-    # data.drop(['months_as_customer'], axis=1, inplace=True) 
-    
-    
+def scaler_trainer(data, output_path):
+    '''
+    Fit the scaler on predictors
+    :params: data, output_path
+    :return: scaler
+    '''
+    scaler = MinMaxScaler()
+    scaler.fit(data)
+    joblib.dump(scaler, output_path)
+    return scaler
+  
+def scaler_trasformer(data, scaler):
+    '''
+    Trasform the data 
+    :params: data, scaler
+    :return: DataFrame
+    '''
+    scaler = joblib.load(scaler) 
+    return scaler.transform(data)
 
-
-
-
-
-
-
+def selector(data, features_selected):
+    '''
+    Select Features
+    :params: data, features_selected
+    :return: DataFrame
+    '''
+    return data[features_selected]
 
 def data_splitter(data, target):
     '''
@@ -109,3 +101,39 @@ def data_splitter(data, target):
                                                         random_state=0)
     return X_train, X_test, y_train, y_test
     
+def model_trainer(data, target, output_path):
+    '''
+    Train the model and store it
+    :params: data, target, output_path
+    :return: None
+    '''
+    # initialise the model
+    rfor = RandomForestClassifier(max_depth=25, 
+                                  min_samples_split=5, 
+                                  n_estimators=300,
+                                  random_state=8)
+       
+    # train the model
+    rfor.fit(data, target)
+    
+    # save the model
+    initial_type = [('features_input', FloatTensorType([1, data.shape[1]]))]
+    onnx = convert_sklearn(rfor, name='rf_champion', initial_types=initial_type)
+    with open(output_path + "rf_champion.onnx", "wb") as f:
+    f.write(onnx.SerializeToString())
+    return None
+
+def model_scorer(data, model):
+    '''
+    Score new data with onnx
+    :params: data, model
+    :return: list
+    '''
+    sess = rt.InferenceSession(model)
+
+    input_name = sess.get_inputs()[0].name
+    label_name = sess.get_outputs()[0].name
+
+    score = np.array(data[:1], dtype=np.float32)
+    predictions_onnx = sess.run([label_name], {input_name: score})
+    return predictions_onnx[0]
