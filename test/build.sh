e682922016ea5a16f295ab583b65af7564e14ec0
ivnard <ivan.nardini@sas.com>
2020-07-03 14:27:15 +0200

    build test

--- test/build.sh
+++ test/build.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
 IMAGE_NAME=$1
+echo "$IMAGE_NAME"
 
-cd /procedural_ml_pipe
-echo "Building docker image..."
+# cd /procedural_ml_pipe
+# echo "Building docker image..."
 
-docker build -t $IMAGE_NAME --rm .
+# docker build -t "$IMAGE_NAME" --rm .
