afc6a4bef73bad500c6401551c447756c84ccb5f
ivnard <ivan.nardini@sas.com>
2020-07-04 09:26:26 +0200

    Test preprocess module

--- test/procedural_ml_pipe/preprocess.py
+++ test/procedural_ml_pipe/preprocess.py
@@ -49,67 +49,67 @@ def dumminizer(data, columns_to_dummies):
     data = pd.get_dummies(data, columns=columns_to_dummies)
     return data
 
-# def scaler_trainer(data, output_path):
-#     '''
-#     Fit the scaler on predictors
-#     :params: data, output_path
-#     :return: scaler
-#     '''
-#     scaler = MinMaxScaler()
-#     scaler.fit(data)
-#     joblib.dump(scaler, output_path)
-#     return scaler
+def selector(data, features_selected):
+    '''
+    Select Features
+    :params: data, features_selected
+    :return: DataFrame
+    '''
+    return data[features_selected]
+
+def data_splitter(data, target):
+    '''
+    Split data in train and test samples
+    :params: DataFrame, target name
+    :return: X_train, X_test, y_train, y_test
+    '''
+    X_train, X_test, y_train, y_test = train_test_split(data,
+                                                        data[target],
+                                                        test_size=0.1,
+                                                        random_state=0)
+    return X_train, X_test, y_train, y_test
+
+def scaler_trainer(data, output_path):
+    '''
+    Fit the scaler on predictors
+    :params: data, output_path
+    :return: scaler
+    '''
+    scaler = MinMaxScaler()
+    scaler.fit(data)
+    joblib.dump(scaler, output_path)
+    return scaler
   
-# def scaler_trasformer(data, scaler):
-#     '''
-#     Trasform the data 
-#     :params: data, scaler
-#     :return: DataFrame
-#     '''
-#     scaler = joblib.load(scaler) 
-#     return scaler.transform(data)
-
-# def selector(data, features_selected):
-#     '''
-#     Select Features
-#     :params: data, features_selected
-#     :return: DataFrame
-#     '''
-#     return data[features_selected]
-
-# def data_splitter(data, target):
-#     '''
-#     Split data in train and test samples
-#     :params: DataFrame, target name
-#     :return: X_train, X_test, y_train, y_test
-#     '''
-#     X_train, X_test, y_train, y_test = train_test_split(data,
-#                                                         data[target],
-#                                                         test_size=0.1,
-#                                                         random_state=0)
-#     return X_train, X_test, y_train, y_test
+def scaler_trasformer(data, scaler):
+    '''
+    Trasform the data 
+    :params: data, scaler
+    :return: DataFrame
+    '''
+    scaler = joblib.load(scaler) 
+    return scaler.transform(data)
     
-# def model_trainer(data, target, output_path):
-#     '''
-#     Train the model and store it
-#     :params: data, target, output_path
-#     :return: None
-#     '''
-#     # initialise the model
-#     rfor = RandomForestClassifier(max_depth=25, 
-#                                   min_samples_split=5, 
-#                                   n_estimators=300,
-#                                   random_state=8)
+def model_trainer(data, target, output_path):
+    '''
+    Train the model and store it
+    :params: data, target, output_path
+    :return: None
+    '''
+    # initialise the model
+    rfor = RandomForestClassifier(max_depth=25, 
+                                  min_samples_split=5, 
+                                  n_estimators=300,
+                                  random_state=8)
        
-#     # train the model
-#     rfor.fit(data, target)
+    # train the model
+    rfor.fit(data, target)
     
-#     # save the model
-#     initial_type = [('features_input', FloatTensorType([1, data.shape[1]]))]
-#     onnx = convert_sklearn(rfor, name='rf_champion', initial_types=initial_type)
-#     with open(output_path + "rf_champion.onnx", "wb") as f:
-#     f.write(onnx.SerializeToString())
-#     return None
+    # save the model
+    initial_type = [('features_input', FloatTensorType([1, data.shape[1]]))]
+    onnx = convert_sklearn(rfor, name='rf_champion', initial_types=initial_type)
+    with open(output_path + "rf_champion.onnx", "wb") as f:
+    f.write(onnx.SerializeToString())
+    return None
 
 # def model_scorer(data, model):
 #     '''
@@ -160,8 +160,25 @@ if __name__ == '__main__':
     for var, meta in config['binning_meta'].items():
         binning_meta = meta
         data[binning_meta['var_name']] = binner(data, var, binning_meta['var_name'], binning_meta['bins'], binning_meta['bins_labels'])
-        
-        if var not in list(data.columns):
-            print('ok')
-        
-        print( data[binning_meta['var_name']])
+    
+    data = dumminizer(data, config['nominal_predictors'])
+
+    data = selector(data, config['features_selected'])
+
+    X_train, X_test, y_train, y_test = data_splitter(data, config['target'])
+    
+    scaler = scaler_trainer(X_train, '.')
+
+    X_train = scaler.transform(X_train)
+
+    model_trainer(X_train, y_train, '.')
+
+    print('Finished training')
+
+
+
+
+
+
+
+    
