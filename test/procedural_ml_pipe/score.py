78b42bc34e5e44b457ac72ef3a9fe61104a6f3a5
ivnard <ivan.nardini@sas.com>
2020-07-05 17:35:24 +0200

    test scoring

--- test/procedural_ml_pipe/score.py
+++ test/procedural_ml_pipe/score.py
@@ -68,10 +68,12 @@ if __name__ == '__main__':
     X_train, X_test, y_train, y_test = train_test_split(data,
                                                         data[config['target']])
     
-    row_to_score = pd.DataFrame([X_test.iloc[0,:]])
+    # row_to_score = pd.DataFrame([X_test.iloc[0,:]])
+
+    data_to_score = X_test
 
     logging.basicConfig(format='%(asctime)s %(levelname)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p', level=logging.INFO)
     logging.info('Scoring process started!')
-    prediction = score(row_to_score)
+    prediction = score(data_to_score)
     logging.info('Scoring finished!')
     logging.info('The prediction label is {}'.format(prediction))
