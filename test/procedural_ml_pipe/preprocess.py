9aba545203c7d715eebbae5c8564aa8ceb5e5c4b
ivnard <ivan.nardini@sas.com>
2020-07-06 17:31:11 +0200

    debugging

--- test/procedural_ml_pipe/preprocess.py
+++ test/procedural_ml_pipe/preprocess.py
@@ -75,6 +75,9 @@ def dumminizer(data, columns_to_dummies, dummies_meta):
         else:
             #check missing labels
             cat_miss_labels = ["_".join([var, cat]) for cat in cat_names if cat not in obs_cat_names]
+            print(var)
+            print(cat_names)
+            print(obs_cat_names)
             #for each labels, create a variables
             for cat in cat_miss_labels:
                 data[cat] = 0
